0102441df028abb795cf7fe4dbb9ffcd97be070f
huusangcv <huusangcv@gmail.com>
2024-08-24 22:57:50 +0700

    UI Phần Header

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,12 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
-
+import images from '~/assets/images';
 const cx = classNames.bind(styles);
 
 const Header = () => {
     return (
         <header className={cx('wapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Search accounts and videos" spellCheck={false} />
+
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>actions</div>
+            </div>
         </header>
     );
 };
